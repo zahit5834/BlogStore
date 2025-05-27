29bf846aa314cdae4570de555fecd6ddff257298
Mehmet <mehmetzahir5834@gmail.com>
2025-05-27 22:10:06 +0300

    Add project files.

---
+++ BlogStore.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using BlogStore.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.DataAccessLayer.Context
+{
+    public class BlogContext:DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-9D6BOD8;initial catalog=BlogStoreNightDb;integrated Security=true;trust server certificate=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ BlogStore.EntityLayer/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.EntityLayer.Entities
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string UserNameSurname { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string CommentDetail { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
